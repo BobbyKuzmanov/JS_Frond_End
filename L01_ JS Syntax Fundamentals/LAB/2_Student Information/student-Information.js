5f30d956afe9f8064eb480d9461248372368be6f
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-02-20 13:57:11 +0200

    Add files via upload

---
+++ L01_ JS Syntax Fundamentals/LAB/2_Student Information/student-Information.js
@@ -0,0 +1,10 @@
+function solve(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+solve('John', 15, 5.54678);
+
+// You will be given 3 parameters – student name (string), age (number), and average grade (number).
+// Your task is to print all the info about the student in the following format:
+// `Name: {student name}, Age: {student age}, Grade: {student grade}`
+//  Note: The grade should be formatted to the second decimal point.
