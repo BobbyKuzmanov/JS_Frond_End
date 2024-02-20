b4820902f5e8d528697457442a39e806810acbed
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-02-20 13:59:05 +0200

    Add files via upload

---
+++ L01_ JS Syntax Fundamentals/LAB/3_Excellent Grade/excellent-grade.js
@@ -0,0 +1,13 @@
+function solve(grade) {
+    if (grade >= 5.5) {
+        console.log("Excellent");
+    } else {
+        console.log("Not excellent");
+    }
+}
+
+solve(5.5);
+solve(4.5);
+
+// Write a function that receives a single number and checks if the grade is excellent or not.
+// If it is, print "Excellent", otherwise print "Not excellent".
