c1f6f9de51db7cb0c58c16803f554968a0ca5de3
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-02-21 13:30:14 +0200

    Add files via upload

---
+++ L01_ JS Syntax Fundamentals/LAB/9_Numbers from 1 to 5/numbers-from-1-to-5.js
@@ -0,0 +1,10 @@
+function solve() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+solve(5);
+
+
+// Write a function that prints all the numbers from 1 to 5 (inclusive) each on a separate line.
