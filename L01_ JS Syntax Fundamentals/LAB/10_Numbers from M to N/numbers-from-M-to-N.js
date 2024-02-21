7d29c4a8b603ce38047b0c73ceff092348c556e5
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-02-21 13:31:03 +0200

    Add files via upload

---
+++ L01_ JS Syntax Fundamentals/LAB/10_Numbers from M to N/numbers-from-M-to-N.js
@@ -0,0 +1,10 @@
+function solve(m, n) {
+    for (let i = m; i >= n; i--) {
+        console.log(i);
+    }
+}
+
+solve(6, 2);
+
+// Write a function that receives a number M and a number N (M will always be bigger than N).
+// Print all numbers from M to N.
