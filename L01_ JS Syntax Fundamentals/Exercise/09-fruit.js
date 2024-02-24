b94669d445f58add18d1e3bc3b4c7d5362c612db
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-02-24 11:59:06 +0200

    Add files via upload

---
+++ L01_ JS Syntax Fundamentals/Exercise/09-fruit.js
@@ -0,0 +1,17 @@
+function solve(fruit, weightInGrams, pricePerKg) {
+  const weight = weightInGrams / 1000;
+  const money = weight * pricePerKg;
+  console.log(`I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`);
+
+}
+
+solve('orange', 2500, 1.80);
+
+// Write a function that calculates how much money you need to buy fruit.
+// You will receive a string for the type of fruit you want to buy, a number for
+// weight in grams, and another number for the price per kilogram.
+// Print the following text on the console:
+// `I need ${money} to buy {weight} kilograms {fruit}.`
+// Print the weight and the money rounded to two decimal places.
+// The input comes as three arguments passed to your function.
+// The output should be printed on the console.
