be668d4939abe9146a7abd5a226b0cc8d7aa1715
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-02-27 08:20:11 +0200

    Add files via upload

---
+++ L02_Array_and_string/Lab/04-substring.js
@@ -0,0 +1,17 @@
+function solve(string, startIndex, count) {
+  let result;
+
+  result = string.substring(startIndex, startIndex + count);
+
+  console.log(result);
+}
+
+solve('ASentence', 1, 8)
+
+
+
+
+// Write a function that receives a string and two numbers. 
+// The numbers will be a starting index and count of elements to substring. 
+// Print the result.Create a new string that takes the needed amount of elements
+// from the given string.
