9e91556d83921c0c734dba13a5b20cee425ac796
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-03-25 08:54:24 +0200

    Add files via upload

---
+++ L05_DOM_Instructions/Lab/04.Show More/ShowMore.js
@@ -0,0 +1,5 @@
+function showText() {
+    const hiddenText = document.getElementById('text');
+    hiddenText.style.display = 'inline';
+    document.getElementById('more').style.display = 'none';
+}
