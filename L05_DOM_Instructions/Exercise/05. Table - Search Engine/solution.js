0d039c1ead381a3b3464a760851f45ab65d8f4ee
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-04-03 08:52:22 +0300

    Add files via upload

---
+++ L05_DOM_Instructions/Exercise/05. Table - Search Engine/solution.js
@@ -0,0 +1,29 @@
+function solve() {
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+   function onClick() {
+
+      const trElements = document.querySelectorAll('table.container tbody tr');
+      const searchFieldElement = document.getElementById('searchField');
+      const searchText = searchFieldElement.value;
+
+      for (const trElement of trElements) {
+         const tdElements = trElement.querySelectorAll('td');
+         let isSelected = false;
+
+         trElement.classList.remove('select');
+
+         for (const tdElement of tdElements) {
+            if (tdElement.textContent.includes(searchText)) {
+               isSelected = true;
+               break;
+            }
+         }
+
+         if (isSelected) {
+            trElement.className = 'select';
+         }
+      }
+      searchFieldElement.value = '';
+   }
+}
